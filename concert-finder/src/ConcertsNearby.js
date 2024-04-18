08f8c135f44f8b12018371465529a9bac9b9b130
Jennica Artica <123128104+jennica-artica@users.noreply.github.com>
2024-04-18 18:34:17 -0400

    trying to make a api call to ticketmaster...it's not working at the moment

--- concert-finder/src/ConcertsNearby.js
+++ concert-finder/src/ConcertsNearby.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import './ConcertsNearby.css';
 import ConcertListing from './ConcertListing';
 
 const ConcertsNearby = () => {
+    const [concerts, setConcerts] = useState([])
+
+    useEffect(() => {
+        fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=1SEJhoe033bJEB4YcShG5T5CzLsmjHqs&keyword=usher&locale=*&city=boston')
+            .then((results) => {
+                return results.json();
+        })
+        .then((data) => {
+            console.log(data);
+            setConcerts(data);
+        });
+    }, []);
     return (
-        <div className='concertsNearby'>
+        /* <div className='concertsNearby'>
             <Header></Header>
             <div className='title'>Concerts Near You</div>
             <ConcertListing
@@ -15,6 +27,13 @@ const ConcertsNearby = () => {
                 time="4pm"
             > 
             </ConcertListing>
+        </div> */
+        <div>
+            {concerts.map((concert) => (
+                <ConcertListing>
+                    artistName=concert.id
+                </ConcertListing>
+            ))}
         </div>
     )
 }
