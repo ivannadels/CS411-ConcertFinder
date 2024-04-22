ca2b9aa846d500a4500b7eaffbe11f7ef21a9611
Jennica Artica <123128104+jennica-artica@users.noreply.github.com>
2024-04-22 15:18:30 -0400

    figured out the results were returning arrays within arrays, so to get each concert listing, we have to iterate through the arrays at each result positon.

--- concert-finder/src/ConcertsNearby.js
+++ concert-finder/src/ConcertsNearby.js
@@ -22,6 +22,7 @@ const ConcertsNearby = () => {
         const fetchForAllArtists = async () => {
             const allConcerts = await Promise.all(
                 testData.map((artistName) => fetchForSingleArtist(artistName)));
+            console.log(allConcerts)
             setConcerts(allConcerts.flat());
         };
         
@@ -34,17 +35,21 @@ const ConcertsNearby = () => {
             <Header></Header>
             <div className='title'>Concerts Near You</div>
             <div>
-                {concerts.map((concert) => (
-                    <ConcertListing
-                        id={concert.id}
-                        artistName={concert._embedded?.attractions?.[0]?.name || 'Unknown Artist'}
-                        location={concert._embedded?.venues?.[0]?.name || 'Unknown Venue'}
-                        city={concert._embedded?.venues?.[0]?.city?.name || 'Unknown City'}
-                        state={concert._embedded?.venues?.[0]?.state?.name || 'Unknown State'}
-                        datetime={concert.dates?.start?.dateTime || 'Unknown Date'}
-                        url={concert.url || '#'}
-                    >
-                    </ConcertListing> 
+                {concerts.map((concert, index) => (
+                    <div>
+                        {concert._embedded?.events.map((concert) => (
+                            <ConcertListing
+                                id={concert.id}
+                                artistName={concert._embedded?.attractions?.[0]?.name || 'Unknown Artist'}
+                                location={concert._embedded?.venues?.[0]?.name || 'Unknown Venue'}
+                                city={concert._embedded?.venues?.[0]?.city?.name || 'Unknown City'}
+                                state={concert._embedded?.venues?.[0]?.state?.name || 'Unknown State'}
+                                datetime={concert.dates?.start?.dateTime || 'Unknown Date'}
+                                url={concert.url || '#'}
+                            >
+                            </ConcertListing>
+                        ))}
+                    </div>
                 ))} 
             </div>
             
